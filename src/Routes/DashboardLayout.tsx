e30130db2cdd8a0a67762d2c4a5bdaada5ad95ef
Tk-brainCodes <ikuohoneboreimethankgod@gmail.com>
2023-05-13 17:45:21 +0100

    feat: dashboard layout component

---
+++ src/Routes/DashboardLayout.tsx
@@ -0,0 +1,15 @@
+import Sidebar from "../Components/Sidebar";
+import { Outlet } from "react-router";
+
+const DashboardLayout = () => {
+  return (
+    <div className='flex'>
+      <Sidebar />
+      <div className='w-[100%] mt-[8vh] bg-[#F0F4FB] min-h-[100vh]'>
+        <Outlet />
+      </div>
+    </div>
+  );
+};
+
+export default DashboardLayout;
